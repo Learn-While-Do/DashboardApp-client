9e958bd0a650b9238cec5cea90ac0bf8e2b1b74f
Learn-While-Do <LearnWhileDo@gmail.com>
2024-06-04 16:37:51 +0200

    End of Chapter 3

---
+++ src/api/reporting.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosResponse} from "axios";
+
+export const loadOrders = () => {
+    return axios.get('http://localhost:8000/api/orders/')
+    .then((response: AxiosResponse) => {
+        
+        console.log(response.data);
+        
+        return response.data;
+    }) 
+}
